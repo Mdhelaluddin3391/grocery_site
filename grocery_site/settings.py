0bd282dc42403a14c498b5c3d060c9ee23c70111
Md Helal Uddin <muhammadhelal228@gmail.com>
2025-10-26 23:10:59 +0530

    some change

--- grocery_site/settings.py
+++ grocery_site/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'store.context_processors.footer_categories',
+                'store.context_processors.main_categories_processor', # <-- YEH LINE ADD KAREIN
                 'cart.context_processors.cart_item_count',
             ],
         },
--- store/context_processors.py
+++ store/context_processors.py
@@ -2,14 +2,21 @@
 
 from .models import Category
 
+def get_main_categories():
+    """Helper function to get main categories for the header."""
+    return Category.objects.filter(parent=None)
+
+def main_categories_processor(request):
+    """
+    Header mein dikhane ke liye main categories.
+    """
+    return {'main_categories': get_main_categories()}
+
 def footer_categories(request):
     """
     Makes all categories available to every template for the footer.
     """
-    # Yahan hum sabhi categories ko fetch kar rahe hain
     all_categories = Category.objects.all() 
-    
-    # Ek dictionary return karein
     return {
         'footer_all_categories': all_categories
     }
